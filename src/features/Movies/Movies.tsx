40d8bfc0dd7150fcda0f4966f7888da0ae2b9dcf
Serg1853 <sergey24226@gmail.com>
2024-04-10 21:05:11 +0300

    fetch

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -3,6 +3,39 @@ import { RootState } from "../../store";
 import { connect } from "react-redux";
 import { MovieCard } from "./MovieCard";
 import styles from "./Movies.module.scss";
+import { useEffect, useState } from "react";
+
+async function getNowPlaying() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDc1YTZkMDQ0NTMxMGNhMTI5NDlhYzA0MTBlNDllMCIsInN1YiI6IjY2MTZjZGZmODU4Njc4MDE0YWM2MjViYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._BYRtOTDR3rksCvRNLC0FpEehVfHBr-77Lg5pLJjXZQ",
+    },
+  };
+
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/now_playing?page=1",
+    options
+  );
+  const json = response.json();
+  return json;
+}
+
+export function MoviesFetch() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await getNowPlaying();
+      setMovies(response.results);
+    }
+    loadData();
+  }, []);
+
+  return <Movies movies={movies} />;
+}
 interface MoviesProps {
   movies: Movie[];
 }
