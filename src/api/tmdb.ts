9110f8bb15589ff4b307522993e1f0679f562fc7
Serg1853 <sergey24226@gmail.com>
2024-04-10 21:30:41 +0300

    fix fetch

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,19 +1,23 @@
+async function get(relativeUrl: string) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDc1YTZkMDQ0NTMxMGNhMTI5NDlhYzA0MTBlNDllMCIsInN1YiI6IjY2MTZjZGZmODU4Njc4MDE0YWM2MjViYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._BYRtOTDR3rksCvRNLC0FpEehVfHBr-77Lg5pLJjXZQ",
+    },
+  };
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3${relativeUrl}`,
+    options
+  );
+  const json = response.json();
+  return json;
+}
+
 export const client = {
   async getNowPlaying() {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDc1YTZkMDQ0NTMxMGNhMTI5NDlhYzA0MTBlNDllMCIsInN1YiI6IjY2MTZjZGZmODU4Njc4MDE0YWM2MjViYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._BYRtOTDR3rksCvRNLC0FpEehVfHBr-77Lg5pLJjXZQ",
-      },
-    };
-
-    const response = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      options
-    );
-    const json = response.json();
-    return json;
+    return await get("/movie/now_playing?page=1");
   },
 };
